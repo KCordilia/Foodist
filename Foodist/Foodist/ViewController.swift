e2928db704c1c41a99984c440d464ce2d776a4e0
KCordilia <k.cordilia@hotmail.com>
2019-07-17 12:56:54 +0200

    Project setup

---
+++ Foodist/Foodist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodist
+//
+//  Created by Karim Cordilia on 17/07/2019.
+//  Copyright © 2019 Karim Cordilia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
