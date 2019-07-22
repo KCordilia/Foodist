62ca497d974d2e61636a6d106284a71f8cc7afb0
Namitha <namithapavithran94@gmail.com>
2019-07-22 13:00:21 +0200

    Implementation not finished - adding result type to network handler, making endpoint dynamic

--- Foodist/Networking/NetworkEndpoints.swift
+++ Foodist/Networking/NetworkEndpoints.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+protocol Preference {
+    
+}
+enum SearchParams {
+    case type
+    case cuisine
+    case intolerances
+    case diet
+}
+enum FoodType: String {
+    case mainCourse = "main+course",sideDish = "side+dish", dessert, appetizer, salad, bread, breakfast, soup, beverage, sauce, drink
+}
+enum CuisineType: String {
+    case african, chinese, japanese, korean, vietnamese, thai, indian, british, irish, french, italian, mexican, spanish, middleEastern = "middle+eastern", jewish, american, cajun, southern, greek, german, nordic, easterneuropean, caribbean,latinAmerican = "latin+american"
+}
+enum Intolerances {
+    case dairy, egg, gluten, peanut
+}
+enum Diet: String {
+    case pescetarian, lactoVegetarian = "lacto+vegetarian", ovoVegetarian = "ovo+vegetarian", vegan, vegetarian
+}
+struct NetworkEndpoint {
+    let recipeSearch = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?type="
+    let recipeImage = "https://spoonacular.com/recipeImages/"
+    let ingredients = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"
+    
+    func getAllSearchParams()-> [SearchParams] {
+        var searchParams = [SearchParams]()
+        searchParams.append(.type)
+        searchParams.append(.cuisine)
+        searchParams.append(.intolerances)
+        return searchParams
+    }
+    
+     func getSearchUrl(for catagory: CuisineType) -> String {
+        var searchType = ""
+      //  case .failure(let error) = result
+        if catagory == CuisineType.middleEastern {
+            searchType = catagory.rawValue
+        }
+        if catagory ==  CuisineType.latinAmerican {
+            searchType = catagory.rawValue
+        }
+        else {
+            searchType = "\(catagory)"
+        }
+        
+        let url = recipeSearch + searchType
+        return url
+    }
+    
+    func getCombinedUrl() -> String {
+        return ""
+    }
+}
--- Foodist/Networking/NetworkHandler.swift
+++ Foodist/Networking/NetworkHandler.swift
@@ -8,15 +8,53 @@
 
 import Foundation
 
+/**
+ enum CatError: Error {
+ case catArrayMissing
+ }
+ 
+ func downloadCats(_ url: String, completion: (Result<[Cat], CatError>) -> Void) {
+ //....
+ let arrayOfCats = [Cat(name: "Suzi"), Cat(name: "Tom")]
+ // good result
+ completion(.success(arrayOfCats))
+ // error case
+ completion(.failure(.catArrayMissing))
+ }
+ 
+ downloadCats("http://") { result in
+ if case .failure(let error) = result {
+ print(error)
+ }
+ guard
+ case .success(let value) = result
+ else { return }
+ print(value)
+ }**/
+
+enum NetworkError {
+    case tooSlow
+    case serverDown
+}
+
 struct NetworkHandler {
+    var APIKey: String!
+    var hostHeader: String!
+    
+   mutating func setUp() {
+        APIKey = "6d4b0c4e3bmsh16b8d0615ae873bp1510ccjsnc4cc6ac1e186"
+        hostHeader = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+    }
+    
     func getAPIData<T: Codable>(_ url: String, completion: @escaping (T?) -> Void) {
+        
         let session = URLSession.shared
         guard
             let url = URL(string: url)
             else { return }
         var request = URLRequest(url: url)
-        request.addValue("6d4b0c4e3bmsh16b8d0615ae873bp1510ccjsnc4cc6ac1e186", forHTTPHeaderField: "X-RapidAPI-Key")
-        request.addValue("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com", forHTTPHeaderField: "X-RapidAPI-Host")
+        request.addValue(APIKey, forHTTPHeaderField: "X-RapidAPI-Key")
+        request.addValue(hostHeader, forHTTPHeaderField: "X-RapidAPI-Host")
         let task = session.dataTask(with: request) { (data, response, error) in
             if error == nil {
                 if let data = data {
